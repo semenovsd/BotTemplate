b62f6a566edf6d57823d1b3382b073c5a307441c
Stanislav Semenov <semenov_sd@bk.ru>
2022-03-19 12:17:54 +0300

    update config and cli

--- tgbot/config.py
+++ tgbot/config.py
@@ -3,40 +3,55 @@ from pathlib import Path
 
 from decouple import config, Csv
 
+@dataclass
+class Telegram:
+    # Telegram settings
+    TGBOT_TOKEN: str = config('TGBOT_TOKEN', cast=str)
+    TG_ADMINS_ID: list = config('TG_ADMINS_ID', cast=Csv(delimiter=':'), default='395415524')
+    # Telegram webhook
+    DOMAIN_NAME_OR_IP: str = config('DOMAIN_NAME_OR_IP', cast=str, default='127.0.0.1')
+    WEBHOOK_PORT: str = config('WEBHOOK_PORT', cast=str, default='8443')
+    WEBHOOK_PATH: str = config('WEBHOOK_PATH', cast=str, default='/bottemplate')
+    WEBHOOK_URL: str = f'{DOMAIN_NAME_OR_IP}:{WEBHOOK_PORT}{WEBHOOK_PATH}'
+    # Webhost
+    TGBOT_HOST: str = config('TGBOT_HOST', cast=str, default='0.0.0.0')
+    TGBOT_PORT: str = config('TGBOT_PORT', cast=str, default='8443')
 
-# Environment
-ENV_STAGE = config('ENV_STAGE', cast=str, default='local')
 
-# SSL settings
-BASE_DIR = Path(__file__).resolve().parent.parent
+@dataclass
+class PostgresConfig:
+    POSTGRES_HOST = config('POSTGRES_HOST', cast=str, default='127.0.0.1')
+    POSTGRES_PORT = config('POSTGRES_PORT', cast=str, default='5432')
+    POSTGRES_USER = config('POSTGRES_USER', cast=str, default='postgres')
+    POSTGRES_PASSWORD = config('POSTGRES_PASSWORD', cast=str, default='postgres')
+    POSTGRES_DB = config('POSTGRES_DB', cast=str, default='tgbot_db')
+    POSTGRES_URI = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
-SSL_DIR = config('SSL_DIR', cast=str, default='./.ssl')
-SSL_CERT = config('SSL_CERT', cast=str, default='url_cert.pem')
-SSL_PRIV = config('SSL_PRIV', cast=str, default='url_private.key')
-SSL_CERT_PATH = os.path.join(BASE_DIR, f'tgbot/{SSL_DIR}{SSL_CERT}')
-SSL_PRIV_PATH = os.path.join(BASE_DIR, f'tgbot/{SSL_DIR}{SSL_PRIV}')
 
-# Telegram settings
-TGBOT_TOKEN = config('TGBOT_TOKEN', cast=str)
-TG_ADMINS_ID = config('TG_ADMINS_ID', cast=Csv(delimiter=':'), default='395415524')
+@dataclass
+class Misc:
+    # Environment
+    ENV_STAGE = config('ENV_STAGE', cast=str, default='local')
+    BASE_DIR: os.path = Path(__file__).resolve().parent
+    # SSL
+    SSL_DIR: str = config('SSL_DIR', cast=str, default='./.ssl')
+    SSL_CERT: str = config('SSL_CERT', cast=str, default='url_cert.pem')
+    SSL_PRIV: str = config('SSL_PRIV', cast=str, default='url_private.key')
+    SSL_CERT_PATH: str = os.path.join(BASE_DIR, f'tgbot/{SSL_DIR}{SSL_CERT}')
+    SSL_PRIV_PATH: str = os.path.join(BASE_DIR, f'tgbot/{SSL_DIR}{SSL_PRIV}')
 
-# Telegram webhook
-DOMAIN_NAME_OR_IP = config('DOMAIN_NAME_OR_IP', cast=str, default='127.0.0.1')
-WEBHOOK_PORT = config('WEBHOOK_PORT', cast=str, default='8443')
-WEBHOOK_PATH = config('WEBHOOK_PATH', cast=str, default='/bottemplate')
-WEBHOOK_URL = f'{DOMAIN_NAME_OR_IP}:{WEBHOOK_PORT}{WEBHOOK_PATH}'
 
-# Webhost
-TGBOT_HOST = config('TGBOT_HOST', cast=str, default='0.0.0.0')
-TGBOT_PORT = config('TGBOT_PORT', cast=str, default='8443')
+@dataclass
+class Config:
+    DB: PostgresConfig = PostgresConfig()
+    MISC: Misc = Misc()
+    TELEGRAM: Telegram = Telegram()
 
-# Postgressql
-POSTGRES_HOST = config('POSTGRES_HOST', cast=str, default='127.0.0.1')
-POSTGRES_PORT = config('POSTGRES_PORT', cast=str, default='5432')
-POSTGRES_USER = config('POSTGRES_USER', cast=str, default='postgres')
-POSTGRES_PASSWORD = config('POSTGRES_PASSWORD', cast=str, default='postgres')
-POSTGRES_DB = config('POSTGRES_DB', cast=str, default='tgbot_db')
-POSTGRES_URI = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
-# Clean env vars after set all params
-os.environ.clear()
+def load_config() -> Config:
+    conf = Config()
+    # Clean env vars
+    os.environ.clear()
+    return conf
+
+config = load_config()
--- tgbot/utils/cli.py
+++ tgbot/utils/cli.py
@@ -1,26 +1,33 @@
 import ssl
+import time
 
 from aiogram import Dispatcher
 from aiogram.utils import executor
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from loguru import logger
 
-from tgbot import config
-from tgbot.db.alchemy import setup_db
-from tgbot.loader import scheduler
+from tgbot.config import config
+from tgbot.db.database import create_db
 from tgbot.utils import send_messages, setup_default_commands, scheduler_jobs
 
 
 async def on_startup_webhook(dp: Dispatcher):
     logger.debug('Start on webhook mode')
-
     # Check webhook
     webhook_info = await dp.bot.get_webhook_info()
+
     # If URL is bad
-    if webhook_info.url != config.WEBHOOK_URL:
+    if webhook_info.url != config.TELEGRAM.WEBHOOK_URL:
         # If URL doesnt match current - remove webhook
         await dp.bot.delete_webhook()
+    logger.debug(f'SET WEBHOOK_URL: {config.TELEGRAM.WEBHOOK_URL}')
     # Set new URL for webhook
-    await dp.bot.set_webhook(config.WEBHOOK_URL, certificate=open(config.SSL_CERT_PATH, 'rb').read())
+    # if cert_type == 'verified':
+    #     # For verified cert
+    await dp.bot.set_webhook(config.TELEGRAM.WEBHOOK_URL)
+    # else:
+    #     # For self-singer cert
+    # await dp.bot.set_webhook(config.WEBHOOK_URL, certificate=open(config.SSL_CERT_PATH, 'rb').read())
 
     await on_startup(dp)
 
@@ -34,19 +41,22 @@ async def on_startup_polling(dp: Dispatcher):
 
 async def on_startup(dp: Dispatcher):
     # Send message to admin
-    await send_messages(config.TG_ADMINS_ID[0], f'Bot startup in {config.ENV_STAGE} environment!')
+    await send_messages(f'Bot startup in {config.MISC.ENV_STAGE} environment!', config.TELEGRAM.TG_ADMINS_ID[0])
     # Setup bot commands
     await setup_default_commands(dp)
     # Create connection to db
-    await setup_db()
-    scheduler_jobs()
+    await create_db()
+    # Setup scheduler
+    scheduler = AsyncIOScheduler()
+    scheduler_jobs(scheduler)
+    scheduler.start()
 
 
 async def on_shutdown(dp: Dispatcher):
     # Send message to admin
-    await send_messages(config.TG_ADMINS_ID[0], f'Bot shutdown in {config.ENV_STAGE} environment!')
+    await send_messages(f'Bot shutdown in {config.MISC.ENV_STAGE} environment!', config.TELEGRAM.TG_ADMINS_ID[0])
     # Shutdown Scheduler
-    scheduler.shutdown()
+    # scheduler.shutdown()
     # Close DB connection (if used)
     await dp.storage.close()
     await dp.storage.wait_closed()
@@ -65,16 +75,18 @@ def webhook(dp: Dispatcher, skip_updates: bool = True):
     """
     # Generate SSL context.
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.load_cert_chain(config.SSL_CERT_PATH, config.SSL_PRIV_PATH)
+    logger.debug(f'LOAD SSL CONTEXT: {config.MISC.SSL_CERT_PATH} and {config.MISC.SSL_PRIV_PATH}')
+    context.load_cert_chain(config.MISC.SSL_CERT_PATH, config.MISC.SSL_PRIV_PATH)
+    logger.debug(f'WEBHOOK_PATH: {config.TELEGRAM.WEBHOOK_PATH}')
 
     executor.start_webhook(
         dispatcher=dp,
-        webhook_path=config.WEBHOOK_PATH,
+        webhook_path=config.TELEGRAM.WEBHOOK_PATH,
         on_startup=on_startup_webhook,
         on_shutdown=on_shutdown,
         skip_updates=skip_updates,
-        host=config.TGBOT_HOST,
-        port=config.TGBOT_PORT,
+        host=config.TELEGRAM.TGBOT_HOST,
+        port=config.TELEGRAM.TGBOT_PORT,
         ssl_context=context
     )
 
@@ -82,10 +94,17 @@ def webhook(dp: Dispatcher, skip_updates: bool = True):
 def cli(argv):
     # Import dp from all handlers
     from tgbot.handlers import dp
-
+    logger.debug(f'########### ARGS: {argv}')
     if argv.mode == 'webhook':
         webhook(dp)
     elif argv.mode == 'polling':
         polling(dp)
+    elif argv.mode == 'fake_start':
+        while True:
+            try:
+                logger.debug(f'\n\n ### SERVICE START IN FAKE MODE ###\n\n')
+                time.sleep(60)
+            except KeyboardInterrupt:
+                break
     else:
         raise AttributeError('Please set correct start mode - webhook or polling')
