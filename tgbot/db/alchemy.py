cac67ec40f5632253a12e588ff9f190e971429bb
Stanislav Semenov <semenov_sd@bk.ru>
2022-03-19 12:02:52 +0300

    fix alchemy url

--- tgbot/db/alchemy.py
+++ tgbot/db/alchemy.py
@@ -12,13 +12,22 @@ Session = sessionmaker(expire_on_commit=False, class_=AsyncSession)
 # Documentation
 # https://docs.sqlalchemy.org/en/14/orm/extensions/asyncio.html
 async def setup_db():
+    # For fix some broblems with strong password with special simbyls
+    url = sqlalchemy.engine.url.URL.create(
+        'postgresql+asyncpg',
+        username=config.USER,
+        password=config.PASSWORD,
+        host=config.HOST,
+        port=config.PORT,
+        database=config.DB,
+    )
     # # Using multiple asyncio event loops
     # # https://docs.sqlalchemy.org/en/14/orm/extensions/asyncio.html
     # from sqlalchemy.pool import NullPool
     # engine = create_async_engine(
     #     "postgresql+asyncpg://user:pass@host/dbname", poolclass=NullPool
     # )
-    engine = create_async_engine(config.POSTGRES_URI, echo=True)
+    engine = create_async_engine(url, echo=True)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
